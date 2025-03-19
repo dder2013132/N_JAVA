f6e8143cda9cc138944be807cddb5f3b74617a34
admin <admin@DESKTOP-CDPTHL4>
2025-03-19 17:36:09 +0900

    new

---
+++ HelloWorld/com/yedam/classes/MethodExe2.java
@@ -0,0 +1,69 @@
+package com.yedam.classes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MethodExe2 {
+
+	private List<Product> store; // 필드.
+
+	// 생성자.
+	MethodExe2() {
+		store = new ArrayList<Product>();// new Product[10];
+		store.add(new Product("A001", "지우개", 500));
+		store.add(new Product("B001", "샤프1000", 1000));
+		store.add(new Product("C001", "연필500", 800));
+		store.add(new Product("D001", "지우개", 1000));
+	}
+
+	// 메소드.
+	boolean add(Product prd) {
+		boolean result = store.add(prd);
+		return result;
+	} // end of add(Product prd)
+
+	// 상품이름, ALL
+	List<Product> productList(Product prd) {
+		List<Product> list = new ArrayList<Product>();// new Product[10];
+		for (int i = 0; i < store.size(); i++) {
+			if (prd.getProductName().equals("ALL") //
+					|| store.get(i).getProductName()//
+							.equals(prd.getProductName())) {
+				// 상품가격이 조건으로 추가됨.
+				if (store.get(i).getPrice() >= prd.getPrice()) {
+					list.add(store.get(i));
+				}
+			}
+		}
+		return list;
+	} // end of productList.
+
+	// 삭제 => boolean remove(String code)
+	boolean remove(String code) {
+		for (int i = 0; i < store.size(); i++) {
+			if (store.get(i).getProductCode().equals(code)) {
+				store.remove(i);
+				return true;
+			}
+		}
+		return false;
+	} // end of remove.
+
+	// 수정 => boolean modify(Product prod)
+	boolean modify(Product prod) {
+		for (int i = 0; i < store.size(); i++) {
+			if (store.get(i).getProductCode().equals(prod.getProductCode())) {
+				// 상품명수정.
+				if (prod.getProductName() != null) {
+					store.get(i).setProductName(prod.getProductName());
+				}
+				// 상품가격수정.
+				if (prod.getPrice() != 0) {
+					store.get(i).setPrice(prod.getPrice());
+				}
+				return true;
+			}
+		}
+		return false;
+	} // end of modify.
+}
---
+++ HelloWorld/com/yedam/variable/Member.java
@@ -0,0 +1,73 @@
+package com.yedam.variable;
+
+// public <-> private
+public class Member {
+	// 클래스: 필드(값을 저장)
+	private String name;
+	private int score; //
+
+	// 클래스: 생성자(실체생성)
+	// 컴파일러 기본생성자 생성.
+	// 생성자 overloading.
+	public Member() {
+
+	}
+
+	public Member(String name, int score) {
+		this.name = name;
+		this.score = score;
+	}
+
+	// Set컬렉션의 중복된 값으로 지정.
+	// name, score => hashCode + equals => 동일한값.
+	@Override
+	public int hashCode() {
+		return score;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof Member) {
+			Member target = (Member) obj;
+			return this.name.equals(target.name);
+		}
+		return false;
+	}
+
+	// 클래스: 메소드(기능) = 반환값 메소드명 (매개값)
+	public void showInfo() {
+		System.out.println("이름은 " + name + ", 점수는 " + score);
+	}
+
+	public void setMember(String name, int score) {
+		this.name = name;
+		this.score = score;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	// score 필드의 값을 지정.
+	public void setScore(int score) {
+		if (score < 0 || score > 100) {
+			System.out.println("적정값을 입력하세요.");
+			return; // 메소드의 종료.
+		}
+		this.score = score;
+	}
+
+	// name 필드의 값을 지정.
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	// toString 재정의.
+	public String toString() {
+		return "이름: " + name + ",점수: " + score;
+	}
+}
